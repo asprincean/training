fbf5575887f396b16a64229c5772fb25a3fee439
Adela Sprincean <asprincean@calibrateconsulting.com>
2022-01-18 15:10:43 +0000

    Another code

---
+++ javascript-task/WDT-744.js
@@ -0,0 +1,30 @@
+const name = 'Alex';
+let age = 25;
+let workplace = 'Leicester';
+workplace = 'London';
+
+console.log(`After few months ${name} will live in ${workplace}`);
+
+// let person = 'Mike';
+// let age = 28;
+
+// function myTag(strings, personExp, ageExp) {
+//   let str0 = strings[0]; // "That "
+//   let str1 = strings[1]; // " is a "
+//   let str2 = strings[2]; // "."
+
+//   let ageStr;
+//   if (ageExp > 99){
+//     ageStr = 'centenarian';
+//   } else {
+//     ageStr = 'youngster';
+//   }
+
+//   // We can even return a string built using a template literal
+//   return `${str0}${personExp}${str1}${ageStr}${str2}`;
+// }
+
+// let output = myTag`That ${ person } is a ${ age }.`;
+
+// console.log(output);
+// // That Mike is a youn
