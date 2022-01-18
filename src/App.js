cc65218fe16ff34cbe6aa7cd55dd6c6e27a96280
Adela Sprincean <asprincean@calibrateconsulting.com>
2022-01-18 15:08:25 +0000

    Some code

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Alex';
+  let age = 25;
+  let workplace = 'Leicester';
+  workplace = 'London';
+
+console.log(`After few months ${name} will live in ${workplace}`);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hey there!
         </p>
+        <p>After few months {name} will live in {workplace}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
