15da4fc7555c1ec3510fd98ac750215e329a7852
Adela Sprincean <asprincean@calibrateconsulting.com>
2022-01-19 11:40:10 +0000

    new task

--- src/App.js
+++ src/App.js
@@ -1,33 +1,61 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import ReactDOM from 'react-dom';
 
-function App() {
-  const name = 'Alex';
-  let age = 25;
-  let workplace = 'Leicester';
-  workplace = 'London';
 
-console.log(`After few months ${name} will live in ${workplace}`);
+// function Example() {
+//   const [count, setCount] = useState(0);
+//   return (
+ 
+//     <div>
+//     <p>You clicked {count} times</p>
+//     <p>{count}</p> 
+//     <button onClick={() => setCount(count + 1)}>
+//       Click me
+//     </button>
+//     </div>
+//   );
+// }
 
+// export default Example;
+
+// function App() {
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hey there!
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <li>{number}</li>
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hey there!
-        </p>
-        <p>After few months {name} will live in {workplace}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul>{listItems}</ul>
   );
 }
 
-export default App;
+const numbers = [1, 2, 3, 4, 5];
+ReactDOM.render(
+  <NumberList numbers={numbers} />,
+  document.getElementById('root')
+);
